41fc9de5d5a30e5427b6abbbf88cc7f428dd1829
alimi muiz <mzayotunde@gmail.com>
2021-05-19 14:57:41 +0100

    First Commit

---
+++ js/script.js
@@ -0,0 +1,64 @@
+$(document).ready(function()  {
+    $(window).scroll(function(){
+        if(this.scrollY > 20) {
+            $('.nav-bar').addClass("sticky");
+        }
+        else {
+            $('.nav-bar').removeClass("sticky");
+        }
+        if(this.scroll > 500) {
+            $('.scroll-up-btn').addClass("show");
+        }
+        else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
+    //Toggle menu nav-bar script
+    $(`.menu-btn`).click(function(){
+        $(`.nav-bar .menu`).toggleClass("active")
+        $(`.menu-btn i`).toggleClass("active")
+    });
+
+    //typing animation script
+    var typed = new Typed(".typing", {
+        strings: ["Developer", "Designer", "Programmer"],
+        typeSpeed: 50,
+        backSpeed: 45,
+        loop: true
+    });
+    var typed = new Typed(".typing-2", {
+        strings: ["Developer", "Designer", "Programmer"],
+        typeSpeed: 50,
+        backSpeed: 45,
+        loop: true
+    });
+
+    //owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+
+    });
+});
